01aa270b057081a5007a18c761af78f1456a4071
Mikelmillz <mikelmillz6@gmail.com>
2023-10-22 20:31:46 -0500

    Updating code for PyBank
    Had to add comments to what I was doing in the code.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,37 +1,45 @@
+#importing what we will need.
 import os
 import csv
 
+#reading the file provided.
 bank_csv = os.path.join("Resources", "budget_data.csv")
-# print(bank_csv)
 
+#opening the file that was privided.
 with open(bank_csv) as csvfile:
     csvreader = csv.reader(csvfile,delimiter=',')
     
-
+    #making sure the the header doesn't mess up my code
     next(csvreader)
 
+    #creating empty arrays to hold lists
     profit_loss = []
     months = []
 
-
+    #creating variables to hold the rows.
     for row in csvreader:
         months.append(row[0])
         profit_loss.append(int(row[1]))
-        
+
+    #creating the variable that will store the answers    
     total_months = len(months)
     sum_ammount = sum(profit_loss)
-    ammount = []
-
+    amount = []
+    
+    #creating a for loop that will go through the profit/losses column and store the difference in the amount list.
     for i in range(1, total_months):
-        ammount.append(profit_loss[i] - profit_loss[i-1])
+        amount.append(profit_loss[i] - profit_loss[i-1])
 
-    ave_change = sum(ammount) / len(ammount)
-    max_prof = max(ammount)
-    min_loss = min(ammount)
+    #Making the variable that will hole then average change, max proffit and most lost in a month.
+    ave_change = sum(amount) / len(amount)
+    max_prof = max(amount)
+    min_loss = min(amount)
 
-    max_month = months[ammount.index(max_prof) + 1]
-    min_month = months[ammount.index(min_loss) + 1]
+    #creating the months that are based off the max profit and loss.
+    max_month = months[amount.index(max_prof) + 1]
+    min_month = months[amount.index(min_loss) + 1]
 
+#displaying the data that we are wanting
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {total_months}")
@@ -40,6 +48,7 @@ print(f"Average Change: ${round(ave_change, 2)}")
 print(f"Greatest Increase in Profits: {max_month} ({max_prof})")
 print(f"Greatest Decrease in Profits: {min_month} ({min_loss})")
 
+#creating a file with our output data
 file = open("Export.txt","w")
 file.write("Financial Analysis" + '\n')
 file.write(f"----------------------------\n")
